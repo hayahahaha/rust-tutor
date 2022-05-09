1fec6d0b20140db8d78a1ff7b2ccd7df54f7f1f5
hayahaha <hayahaha@MacBook-Pro.local>
2022-05-09 21:34:59 +0700

    guessing number

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Calculate guessing number...");
+    let secret_number  = rand::thread_rng().gen_range(1..101); 
+
+    println!("Print guessing number, ...!");
+    let mut guess = String::new();
+
+    println!("Secret number is {}", secret_number);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Fail to read line!");
+    println!("You guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("Equal"),
+    }
+
 }
